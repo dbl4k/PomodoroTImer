750217a22084dd495bcbdb8b2c97bb3655d46fef
dbl4k <github@dbl4k.net>
2018-03-11 13:00:43 +0000

    add scheduler models

---
+++ PomodoroTimer/Models/ScheduleItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PomodoroTimer.Models
+{
+    public class ScheduleItem
+    {
+        public String Label { get; set; }
+        public TimeSpan TimeToSpend { get; set; }
+        public bool Completed { get; set; }
+    }
+}
