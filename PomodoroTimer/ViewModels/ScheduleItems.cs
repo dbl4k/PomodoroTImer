5894f6c4925f94542bec7917d592fbe5fcfa1652
dbl4k <github@dbl4k.net>
2018-03-11 14:47:02 +0000

    add new observable collection scheduleitems

---
+++ PomodoroTimer/ViewModels/ScheduleItems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PomodoroTimer.Models;
+
+namespace PomodoroTimer.ViewModels
+{
+    public class ScheduleItems : ObservableCollection<ScheduleItem>
+    {
+        public ScheduleItems() : base()
+        {
+        }
+
+        public ScheduleItems(int items) : base()
+        {
+            for (var i = 1; i <= 5; i++)
+            {
+                this.Add(new ScheduleItem(String.Format("Activity ", i)));
+            }
+   
+        }
+    }
+}
