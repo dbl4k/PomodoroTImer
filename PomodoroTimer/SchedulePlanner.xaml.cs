c061a7c7acf3ddd0651af25ff05bd4560f601b36
dbl4k <github@dbl4k.net>
2018-03-11 13:30:12 +0000

    add scheduleplanner window

---
+++ PomodoroTimer/SchedulePlanner.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PomodoroTimer
+{
+    /// <summary>
+    /// Interaction logic for SchedulePlanner.xaml
+    /// </summary>
+    public partial class SchedulePlanner : Window
+    {
+        public SchedulePlanner()
+        {
+            InitializeComponent();
+        }
+        
+        #region "ui events"
+
+        // draggable window support
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        #endregion
+
+    }
+}
