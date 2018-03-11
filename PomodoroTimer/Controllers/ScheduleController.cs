56cc27d2b3527a936636976ee003150d2cea9506
dbl4k <github@dbl4k.net>
2018-03-11 13:18:43 +0000

    add lisst + item initialization logic to schedule constructors

---
+++ PomodoroTimer/Controllers/ScheduleController.cs
@@ -0,0 +1,57 @@
+﻿using PomodoroTimer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer.Controllers
+{
+    class ScheduleController
+    {
+
+        private static String F_DEFAULT_ITEM_NAME = "Item {0}";
+
+        #region "fields"
+
+        public Schedule _schedule;
+        
+        #endregion
+
+        #region "properties"
+
+        // none yet..
+
+        #endregion
+
+        #region "constructor(s)"
+
+        public ScheduleController() : this(1) 
+        {
+        }
+
+        public ScheduleController(int items)
+        {
+            _schedule = new Schedule();
+            _schedule.Items = new List<ScheduleItem>();
+
+            for(var i=1; i <= items; i++)
+            {
+                var name = GetDefaultItemName(i);
+                _schedule.Items.Add(new ScheduleItem(name));
+            }
+        }
+        
+        #endregion
+
+        #region "methods"
+
+        private string GetDefaultItemName(int i)
+        {
+            return String.Format(F_DEFAULT_ITEM_NAME, i);
+        }
+
+        #endregion
+
+    }
+}
--- PomodoroTimer/Models/ScheduleItem.cs
+++ PomodoroTimer/Models/ScheduleItem.cs
@@ -9,8 +9,32 @@ namespace PomodoroTimer.Models
 {
     public class ScheduleItem
     {
+        private static readonly TimeSpan DEFAULT_TIME_TO_SPEND = TimeSpan.FromMinutes(25);
+
+        #region "properties"
+        
         public String Label { get; set; }
         public TimeSpan TimeToSpend { get; set; }
         public bool Completed { get; set; }
+
+        #endregion
+
+        #region "constructor(s)"
+
+        public ScheduleItem() : this(String.Empty)
+        {
+        }
+
+        public ScheduleItem(String label) : this(label, DEFAULT_TIME_TO_SPEND)
+        {
+        }
+        
+        public ScheduleItem(String label, TimeSpan timeToSpend)
+        {
+            Label = label;
+            TimeToSpend = timeToSpend;
+        }
+
+        #endregion
     }
 }
