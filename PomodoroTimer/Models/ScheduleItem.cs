862121049e257d1d73ae9a2d1301de4b68b7343e
dbl4k <github@dbl4k.net>
2018-03-15 20:13:02 +0000

    implement property change notify to bubble events up to bound controls

--- PomodoroTimer/Models/ScheduleItem.cs
+++ PomodoroTimer/Models/ScheduleItem.cs
@@ -1,21 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PomodoroTimer.Annotations;
 
 namespace PomodoroTimer.Models
 {
-    public class ScheduleItem
+    public class ScheduleItem : INotifyPropertyChanged
     {
         private static readonly TimeSpan DefaultTimeToSpend = TimeSpan.FromMinutes(25);
 
         #region "properties"
-        
-        public String Label { get; set; }
-        public TimeSpan TimeToSpend { get; set; }
-        public bool Completed { get; set; } = false;
+
+        private string _label = null;
+        private TimeSpan _timeToSpend;
+        private bool _completed = false;
+
+        public String Label
+        {
+            get
+            {
+                return _label;
+            }
+            set
+            {
+                _label = value;
+                OnPropertyChanged(nameof(Label));
+            }
+        }
+
+        public TimeSpan TimeToSpend
+        {
+            get
+            {
+                return _timeToSpend;
+            }
+            set
+            {
+                _timeToSpend = value;
+                OnPropertyChanged(nameof(TimeToSpend));
+            }
+        }
+
+        public bool Completed
+        {
+            get
+            {
+                return _completed;
+            }
+            set
+            {
+                _completed = value;
+                OnPropertyChanged(nameof(Completed));
+            }
+        }
 
         #endregion
 
@@ -33,6 +75,18 @@ namespace PomodoroTimer.Models
         {
             Label = label;
             TimeToSpend = timeToSpend;
+        }
+
+        #endregion
+
+        #region "INotifyPropertyChanged implementation"
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         #endregion
